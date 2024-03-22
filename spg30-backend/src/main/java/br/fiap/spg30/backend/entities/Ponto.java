f5371323b2d1285afec6ffb772410a131778a427
Marcus <marcusvcl@gmail.com>
2024-03-21 21:14:54 -0300

    Registro de ponto em rota autenticada

--- spg30-backend/src/main/java/br/fiap/spg30/backend/entities/Ponto.java
+++ spg30-backend/src/main/java/br/fiap/spg30/backend/entities/Ponto.java
@@ -1,6 +1,46 @@
 package br.fiap.spg30.backend.entities;
 
-public class Ponto {
-    private Long id;
-    private String name;
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import io.quarkus.mongodb.panache.common.MongoEntity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@MongoEntity(collection = "pontos")
+public class Ponto extends PanacheMongoEntity {
+    public Long userId;
+    public LocalDate data;
+    public List<LocalDateTime> marcacoes = new ArrayList<>();
+
+    public static List<Ponto> findAllByUserId(Long userId) {
+        return list("userId", userId);
+    }
+
+    public static Ponto findByUserIdAndData(Long userId, LocalDate data) {
+        return find("userId = ?1 and data = ?2", userId, data).firstResult();
+    }
+
+    public static void addMarcacao(Long userId) {
+        Ponto ponto = Ponto.find("userId = ?1 and data = ?2", userId, LocalDate.now()).firstResult();
+        if (Objects.isNull(ponto)) {
+            ponto = Ponto.createFrom(userId, LocalDate.now());
+            ponto.persist();
+        }
+        ponto.marcacoes.add(LocalDateTime.now());
+        ponto.update();
+    }
+
+    public static List<Ponto> findAllBetweenDates(Long userId, LocalDate startDate, LocalDate endDate) {
+        return list("userId = ?1 and data >= ?2 and data <= ?3", userId, startDate, endDate);
+    }
+
+    private static Ponto createFrom(Long userId, LocalDate data) {
+        Ponto ponto = new Ponto();
+        ponto.userId = userId;
+        ponto.data = data;
+        return ponto;
+    }
 }
--- spg30-backend/src/main/java/br/fiap/spg30/backend/resources/PontoResource.java
+++ spg30-backend/src/main/java/br/fiap/spg30/backend/resources/PontoResource.java
@@ -1,29 +1,42 @@
 package br.fiap.spg30.backend.resources;
 
 import br.fiap.spg30.backend.entities.Ponto;
-import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.time.LocalDate;
 
 @Path("/backend/ponto")
 @ApplicationScoped
 public class PontoResource {
 
     @GET
+    @Path("/{userId}/{date}")
     @Produces(MediaType.APPLICATION_JSON)
-    @PermitAll
-    public Response get() {
-        return Response.ok(new Ponto()).build();
+    @RolesAllowed("USER")
+    public Response get(Long userId, LocalDate date) {
+        return Response.ok(Ponto.findByUserIdAndData(userId, date)).build();
+    }
+
+    @GET
+    @Path("/{userId}/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed("USER")
+    public Response getBetweenDates(Long userId) {
+        return Response.ok(Ponto.findAllByUserId(userId)).build();
     }
 
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/{userId}")
     @RolesAllowed("USER")
-    public Response post(Ponto ponto) {
-        return Response.ok(ponto).build();
+    public Response marcarPonto(Long userId) {
+        Ponto.addMarcacao(userId);
+        return Response.ok().build();
     }
 }
